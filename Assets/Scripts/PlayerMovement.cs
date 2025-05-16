5cd936361e9b46a47410a055b17d10869c919ed6
dongdongdongk <dhk9309@gmail.com>
2025-05-16 16:45:07 +0900

    총알 생성

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -11,6 +11,8 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] float jumpSpeed = 10f;
     [SerializeField] float climbSpeed = 5f;
     [SerializeField] Vector2 deathKick = new Vector2(10f, 10f);
+    [SerializeField] GameObject bullet;
+    [SerializeField] Transform gun;
     Vector2 moveInput;
     Rigidbody2D myRigidbody;
     Animator myAnimator;
@@ -36,6 +38,11 @@ public class PlayerMovement : MonoBehaviour
         FlipSprite();
         ClimbLadder();
         Die();
+    }
+
+    void OnFire(InputValue value) {
+        if (!isAlive) { return; }
+        Instantiate(bullet, gun.position, transform.rotation);
     }
 
     void OnMove(InputValue value)
@@ -95,7 +102,7 @@ public class PlayerMovement : MonoBehaviour
     }
 
     void Die() {
-        if (myBodyCollider.IsTouchingLayers(LayerMask.GetMask("Enemy"))) {
+        if (myBodyCollider.IsTouchingLayers(LayerMask.GetMask("Enemy" ,"Hazards"))) {
             isAlive = false;
             myAnimator.SetTrigger("Die");
             myRigidbody.velocity = deathKick;
